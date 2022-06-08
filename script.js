aa2bdc42d1d930139ab2fc81a1eb3e1568ff95e1
pinerose <60974032+pinerose@users.noreply.github.com>
2022-06-08 12:11:28 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,172 @@
+let survivor = [
+    {name: "unlucky", Sskin: 0, Askin: 3, Sacce: 0, Aacce: 1},
+    {name: "doctor", Sskin: 3, Askin: 9, Sacce: 1, Aacce: 4},
+    {name: "lawyer", Sskin: 0, Askin: 5, Sacce: 0, Aacce: 2},
+    {name: "thief", Sskin: 1, Askin: 4, Sacce: 0, Aacce: 4},
+    {name: "gardener", Sskin: 2, Askin: 8, Sacce: 2, Aacce: 3},
+    {name: "magician", Sskin: 1, Askin: 7, Sacce: 1, Aacce: 1},
+    {name: "explorer", Sskin: 1, Askin: 6, Sacce: 0, Aacce: 1},
+    {name: "mercenary", Sskin: 2, Askin: 13, Sacce: 2, Aacce: 1},
+    {name: "coordinator", Sskin: 2, Askin: 10, Sacce: 1, Aacce: 3},
+    {name: "mechanic", Sskin: 3, Askin: 10, Sacce: 4, Aacce: 3},
+    {name: "forward", Sskin: 2, Askin: 10, Sacce: 1, Aacce: 3},
+    {name: "mindEye", Sskin: 3, Askin: 6, Sacce: 0, Aacce: 4},
+    {name: "priestess", Sskin: 4, Askin: 7, Sacce: 1, Aacce: 2},
+    {name: "perfumer", SSskin: 1, Sskin: 2, Askin: 7, Sacce: 3, Aacce: 2},
+    {name: "cowboy", Sskin: 1, Askin: 4, Sacce: 0, Aacce: 2},
+    {name: "dancer", Sskin: 3, Askin: 7, Sacce: 1, Aacce: 2},
+    {name: "seer", Sskin: 3, Askin: 6, Sacce: 1, Aacce: 2},
+    {name: "embalmer", Sskin: 2, Askin: 6, Sacce: 1, Aacce: 2},
+    {name: "prospector", Sskin: 2, Askin: 6, Sacce: 0, Aacce: 2},
+    {name: "enchantress", Sskin: 2, Askin: 5, Sacce: 1, Aacce: 1},
+    {name: "wildling", Sskin: 1, Askin: 3, Sacce: 0, Aacce: 2},
+    {name: "acrobat", Sskin: 3, Askin: 4, Sacce: 0, Aacce: 2},
+    {name: "officer", Sskin: 3, Askin: 3, Sacce: 0, Aacce: 1},
+    {name: "barmaid", Sskin: 2, Askin: 5, Sacce: 0, Aacce: 1},
+    {name: "postman", Sskin: 1, Askin: 4, Sacce: 1, Aacce: 0},
+    {name: "graveyard", Sskin: 1, Askin: 3, Sacce: 0, Aacce: 1},
+    {name: "prisoner", Sskin: 2, Askin: 5, Sacce: 0, Aacce: 1},
+    {name: "entomologist", Sskin: 2, Askin: 2, Sacce: 1, Aacce: 1},
+    {name: "painter", Sskin: 2, Askin: 1, Sacce: 1, Aacce: 1},
+    {name: "batter", Sskin: 1, Askin: 1, Sacce: 0, Aacce: 1},
+    {name: "merchant", Sskin: 1, Askin: 2, Sacce: 0, Aacce: 1},
+    {name: "psychologist", Sskin: 1, Askin: 1, Sacce: 0, Aacce: 0},
+    {name: "patient", Sskin: 1, Askin: 1, Sacce: 0, Aacce: 1},
+    {name: "novelist", Sskin: 1, Askin: 1, Sacce: 0, Aacce: 0},
+    {name: "memory", Sskin: 1, Askin: 1, Sacce: 0, Aacce: 0},
+    {name: "wheepingClown", Sskin: 1, Askin: 1, Sacce: 0, Aacce: 0},
+    {name: "professor", Sskin: 1, Askin: 0, Sacce: 0, Aacce: 0}
+]
+
+let hunter = [
+    {name: "hellEmber", Sskin: 1, Askin: 6, Sacce: 1, Aacce: 1},
+    {name: "joker", Sskin: 3, Askin: 3, Sacce: 1, Aacce: 3},
+    {name: "gameKeeper", Sskin: 2, Askin: 5, Sacce: 0, Aacce: 2},
+    {name: "reaper", Sskin: 4, Askin: 8, Sacce: 1, Aacce: 4},
+    {name: "soulWeaver", Sskin: 2, Askin: 5, Sacce: 1, Aacce: 1},
+    {name: "geisha", Sskin: 4, Askin: 8, Sacce: 1, Aacce: 3},
+    {name: "feaster", Sskin: 2, Askin: 4, Sacce: 1, Aacce: 2},
+    {name: "wuChang", Sskin: 3, Askin: 5, Sacce: 0, Aacce: 4},
+    {name: "photographer", Sskin: 2, Askin: 8, Sacce: 2, Aacce: 2},
+    {name: "madEye", Sskin: 1, Askin: 2, Sacce: 0, Aacce: 2},
+    {name: "dreamWitch", Sskin: 2, Askin: 4, Sacce: 1, Aacce: 1},
+    {name: "axeBoy", Sskin: 2, Askin: 4, Sacce: 1, Aacce: 2},
+    {name: "evilRaptilian", Sskin: 2, Askin: 3, Sacce: 0, Aacce: 2},
+    {name: "mary", Sskin: 4, Askin: 3, Sacce: 1, Aacce: 1},
+    {name: "guard26", Sskin: 3, Askin: 3, SSacce: 1, Sacce: 2, Aacce: 1},
+    {name: "disciple", Sskin: 1, Askin: 2, Sacce: 1, Aacce: 1},
+    {name: "violinist", Sskin: 2, Askin: 2, Sacce: 0, Aacce: 2},
+    {name: "sculptor", Sskin: 2, Askin: 1, Sacce: 1, Aacce: 1},
+    {name: "undead", Sskin: 1, Askin: 1, Sacce: 0, Aacce: 1},
+    {name: "breakingWheel", Sskin: 1, Askin: 1, Sacce: 0, Aacce: 1},
+    {name: "naiad", Sskin: 1, Askin: 0, Sacce: 0, Aacce: 1},
+    {name: "philip", Sskin: 1, Askin: 1, Sacce: 0, Aacce: 0},
+    {name: "nightmare", Sskin: 1, Askin: 1, Sacce: 0, Aacce: 0},
+    {name: "clerk", Sskin: 1, Askin: 0, Sacce: 0, Aacce: 0}
+]
+
+let startIndexedDB = (DBName, DBVersion) => {
+    if (!window.indexedDB){
+        alert("Browser doesn't this site.");
+    } else {
+        let db;
+        let request = indexedDB.open(DBName, DBVersion);
+        request.onupgradeneeded = e => {
+            db = e.target.result;
+            switch (e.oldVersion){
+                case 0: 
+                    for (let x of survivor){
+                        var objectStore = db.createObjectStore(x.name, {keyPath: 'id'});
+                    }
+                    for (let x of hunter){
+                        var objectStore = db.createObjectStore(x.name, {keyPath: 'id'});
+                    }
+                    break;
+            }
+        }
+    }
+}
+
+startIndexedDB("skins", 1);
+
+let setIndexedDB = (skinKinds) => {
+    var request = window.indexedDB.open('skins');
+
+    request.onsuccess = e => {
+        var db = e.target.result;
+        for (let x of survivor){
+            var transaction = db.transaction([x.name], 'readwrite');
+            
+            var objectStore = transaction.objectStore(x.name);
+            for (let y = 0; y < x.Sskin; y++){
+                var request = objectStore.add({id: y+1, name: `${x.name}Sskin${y+1}`, brightness: 0.3});
+                request.onsuccess = e => {
+                    console.log(e.target.result);
+                }
+            };
+        };
+        for (let x of survivor){
+            var transaction = db.transaction([x.name], 'readwrite');
+
+            var objectStore = transaction.objectStore(x.name);
+            for (let y = 0; y < x.Askin; y++){
+                var request = objectStore.add({id: y+1, name: `${x.name}Askin${y+1}`, brightness: 0.3});
+                request.onsuccess = e => {
+                    console.log(e.target.result);
+                }
+            }; 
+        };
+        for (let x of survivor){
+            var transaction = db.transaction([x.name], 'readwrite');
+            
+            var objectStore = transaction.objectStore(x.name);
+            for (let y = 0; y < x.Sacce; y++){
+                var request = objectStore.add({id: y+1, name: `${x.name}Sacce${y+1}`, brightness: 0.3});
+                request.onsuccess = e => {
+                    console.log(e.target.result);
+                }
+            };
+        };
+        for (let x of survivor){
+            var transaction = db.transaction([x.name], 'readwrite');
+            
+            var objectStore = transaction.objectStore(x.name);
+            for (let y = 0; y < x.Aacce; y++){
+                var request = objectStore.add({id: y+1, name: `${x.name}Aacce${y+1}`, brightness: 0.3});
+                request.onsuccess = e => {
+                    console.log(e.target.result);
+                }
+            };
+        };
+    }
+}
+
+    /* request.onsuccess = e => {
+        db = e.target.result;
+        console.log(this.result);
+        for (let x of survivor){
+            let transaction = db.transaction([x.name]);
+
+            transaction.onerror = function(event) {
+                console.log('fail');
+            }
+
+            transaction.oncomplete = function(event) {
+                console.log(result);
+                console.log('done');
+            }
+
+            var objectStore = transaction.objectStore(x.name);
+            var request = objectStore.openCursor();
+            request.onsuccess = e => {
+                var cursor = e.target.result;
+                if (cursor) {
+                    request = objectStore.get(cursor.key);
+                    request.onsuccess = e => {
+                        console.log(e.target.result);
+                    }
+                    cursor.continue();
+                }
+            }
+        }
+    } */
